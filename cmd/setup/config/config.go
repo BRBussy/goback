69e14ca6a1011a26fefe37caee804992f23d3f49
bernardbussy <brbitzbussy@gmail.com>
2020-08-07 15:09:35 +0200

    add config for setu

---
+++ cmd/setup/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetDefault("MongoDBName", "goback")
+}
+
+// Config holds configuration information for the setup
+type Config struct {
+	MongoDBConnectionString string
+	MongoDBHosts            []string
+	MongoDBName             string
+	MongoDBUsername         string
+	MongoDBPassword         string
+}
+
+// GetConfig looks for and tries to parse a .toml config file with the given name
+func GetConfig(configFileName *string) (*Config, error) {
+	// set places to look for config file
+	viper.AddConfigPath("cmd/setup/config")
+	viper.AddConfigPath("config")
+	viper.AddConfigPath(".")
+
+	// set the name of the config file
+	viper.SetConfigName(*configFileName)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Error().Err(err).Msgf("could not parse config file")
+		return nil, err
+	}
+
+	// parse the config file
+	cfg := new(Config)
+	if err := viper.Unmarshal(cfg); err != nil {
+		log.Error().Err(err).Msg("unmarshalling config file")
+		return nil, err
+	}
+
+	return cfg, nil
+}
