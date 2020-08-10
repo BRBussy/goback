0d20999de20777ad207ead3e76b5cc181240c759
bernardbussy <brbitzbussy@gmail.com>
2020-08-10 17:06:58 +0200

    populate authorizer interface

--- pkg/security/authorisation/authorizer.go
+++ pkg/security/authorisation/authorizer.go
@@ -1,4 +1,16 @@
 package authorisation
 
+import "github.com/BRBussy/goback/pkg/security/claims"
+
 type Authorizer interface {
+	ConfirmServiceAccess(ConfirmServiceAccessRequest) (*ConfirmServiceAccessResponse, error)
+}
+
+type ConfirmServiceAccessRequest struct {
+	Claims  claims.Claims `validate:"required"`
+	Service string        `validate:"required"`
+}
+
+type ConfirmServiceAccessResponse struct {
+	Result bool
 }
